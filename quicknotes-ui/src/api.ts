05d9dff299fad9af7b5ec69c4eb652a2f0a3d206
rban8770 <rban8770@uni.sydney.edu.au>
2025-04-29 23:33:13 +1000

    Proxy API calls and basic types/API helpers

---
+++ quicknotes-ui/src/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export interface Note {
+  id: number;
+  text: string;
+  created: string;
+}
+
+export const getNotes  = () => axios.get<Note[]>('/api/notes');
+export const addNote   = (text: string) => axios.post('/api/notes', text);
+export const delNote   = (id: number) => axios.delete(`/api/notes/${id}`);
